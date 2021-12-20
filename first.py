31bfe57b07ab617747fce185dac16d3765a8fbf9
Software Engineering Lab <fidaalibaig2002@gmail.com>
2021-12-20 17:10:18 +0500

    add prsdf;sdfkjsint line

--- first.py
+++ first.py
@@ -14,21 +14,8 @@ def encrypt(text,s):
 
 	return result
 
-def decrypt(text,s):
-    result = ""
-    for i in range(len(text)):
-        char = text[i]
-        if (char.isupper()):
-            result += chr((ord(char) - s-65) % 26 + 65)
-        else:
-            result += chr((ord(char) - s - 97) % 26 + 97)
-    return result
 text = "DEFENDTHEFORT"
 s = 7
 print("origional text : " + text)
 print("Shift : " + str(s))
-print("excrypted text: " + encrypt(text,s))
-x=encrypt(text,s)
-a = input()
-print("decrypted text: " + decrypt(x,s))
 
